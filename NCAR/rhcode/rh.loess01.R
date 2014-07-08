ab8a1613845da1aec053473d8ac759529e87c82e
XiaosuTong <xiaosutong@gmail.com>
2014-07-08 17:10:16 -0400

    update stl for vertices from loess

--- NCAR/rhcode/rh.loess01.R
+++ NCAR/rhcode/rh.loess01.R
@@ -3,9 +3,7 @@
 ##get the loess fit at each vertix of the kd tree using the whole 
 ##dataset. The output key is the vertix id, and value is the 1236 
 ##fitted value for that vertix.
-#
-
-
+##
 ##initialize the rhipe and setup the directories
 source("~/Rhipe/rhinitial.R")
 source("~/Projects/Spatial/NCAR/rhcode/rh.setup.R")
@@ -28,7 +26,10 @@ if(par$dataset == "precip"){
 rm(list=grep("US", ls(), value=T))
 source("~/Projects/Spatial/NCAR/code/spatial/kdtree.R")
 kd <- kdtree(info[, c("station.id","lon","lat")], alpha = par$span/5)
-rhsave(list=("kd"), file=file.path(rh.datadir, par$dataset, "Rdata", paste("kd", ".RData", sep="")))
+rhsave(
+	list = ("kd"), 
+	file = file.path(rh.datadir, par$dataset, "Rdata", paste("kd", ".RData", sep=""))
+)
 
 ##loess fit at each vertices
 job <- list()
@@ -104,12 +105,45 @@ z <- rhex(job.mr, async = TRUE)
 ########################################
 ## STL fitting for each vertex
 ##
+a <- list(sw="periodic", sd=1, tw=241, td=1, inner=10, outer=0, flag=FALSE)
+#a <- list(sw="periodic", sd=1, tw=1855, td=1, fcw=c(1855,241), fcd=c(1,1), inner=10, outer=0, flag=TRUE) 
+par <- c(par, a)
 job <- list()
 job$map <- expression({
-	
+  lapply(seq_along(map.keys), function(r) {
+	tmp <- map.values[[r]]
+	if(par$flag){
+    	dr <- do.call("cbind", stl2(tmp$fitted, tmp$time,
+    		n.p=12,
+    		s.window = par$sw,
+    		s.degree = par$sd,
+    		t.window = par$tw,
+   			t.degree = par$td,
+   			fc.window = par$fcw,
+   			fc.degree = par$fcd,
+   			inner = par$inner,
+    		outer = par$outer
+		)[c("data","fc")])
+		tmp <- cbind(tmp, dr[, c(!(names(dr) %in% c("station.id", "data.raw", "data.sub.labels")))])
+		names(tmp)[grep("fc.fc", names(tmp))] <- c("fc.trend", "fc.second")
+	}else{
+    	dr <- stl2(tmp$fitted, tmp$time,
+    		n.p = 12,
+    		s.window = par$sw,
+    		s.degree = par$sd,
+    		t.window = par$tw,
+    		t.degree = par$td,
+    		inner = par$inner,
+    		outer = par$outer)$data
+		tmp <- cbind(tmp, dr[, c(!(names(dr) %in% c("station.id", "raw", "sub.labels")))])
+	}
+	rhcollect(map.keys[[r]], tmp)
+  })
 })
 job$setup <- expression(
     map = {
+		library(lattice)
+   		library(yaImpute, lib.loc = lib.loc)
 		library(stl2, lib.loc = lib.loc)
     },
 )
@@ -119,7 +153,7 @@ job$output <- rhfmt(file.path(rh.datadir, par$dataset, "spatial", "loess01.stl")
 job$mapred <- list(
 	mapred.reduce.tasks = 66, 
 	rhipe_reduce_buff_size = 10000, 
-	mapred.max.split.size = 150000 # the size of the data frame for one vertex is about this size
+	mapred.max.split.size = 200000 # the size of the data frame for one vertex is about this size
 )
 job$mon.sec <- 5
 job$jobname <- file.path(rh.datadir, par$dataset, "spatial", "loess01.stl")
