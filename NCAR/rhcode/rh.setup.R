8206335373debbf60572404e0c7c93d8e724e522
XiaosuTong <xiaosutong@gmail.com>
2014-09-01 13:12:02 -0400

    update setup.R

--- NCAR/rhcode/rh.setup.R
+++ NCAR/rhcode/rh.setup.R
@@ -1,10 +1,22 @@
 #Set up the directory and load the data.
 library(lattice)
 library(plyr)
-local.datadir <- "/home/tongx/Projects/Spatial/NCAR/RData"
-local.output <- "/home/tongx/Projects/Spatial/NCAR/output"
-rh.datadir <- "/wsc/tongx/Spatial/tmp"
-#The output directory for saving plots should have all permission since the plots are written to the output directory by a user related to hadoop.
-#In the reduce step, the permission for the plots should be changed to be all +wrx.
-rh.output <- "/wsc/tongx/Spatial/output"
-lib.loc <- "/home/tongx/R_LIBS"
+if(par$machine == "rossmann"){
+	local.datadir <- "/home/tongx/Projects/Spatial/NCAR/RData"
+	local.output <- "/home/tongx/Projects/Spatial/NCAR/output"
+	rh.datadir <- "/wsc/tongx/Spatial/tmp"
+	#The output directory for saving plots should have all permission since the plots are written to 
+    #the output directory by a user related to hadoop.
+	#In the reduce step, the permission for the plots should be changed to be all +wrx.
+	rh.output <- "/wsc/tongx/Spatial/output"
+	lib.loc <- "/home/tongx/R_LIBS"
+}else if(par$machine == "gacrux"){
+	local.datadir <- "/home/shaula/u16/tongx/Projects/Spatial/NCAR/RData"
+	local.output <- "/home/shaula/u16/tongx/Projects/Spatial/NCAR/output"
+	rh.datadir <- "/ln/tongx/Spatial/tmp"
+	#The output directory for saving plots should have all permission since the plots are written to 
+    #the output directory by a user related to hadoop.
+	#In the reduce step, the permission for the plots should be changed to be all +wrx.
+	rh.output <- "/ln/tongx/Spatial/output"
+	lib.loc <- "/home/shaula/u16/tongx/R_LIBS"
+}
