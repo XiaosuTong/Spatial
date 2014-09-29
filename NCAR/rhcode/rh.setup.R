e0d132d0420abaac0d2caf52ec3729e597155ea1
XiaosuTong <xiaosutong@gmail.com>
2014-09-29 15:25:54 -0400

    add RawtoHDFS

--- NCAR/rhcode/rh.setup.R
+++ NCAR/rhcode/rh.setup.R
@@ -10,10 +10,11 @@ if(par$machine == "rossmann"){
 	#In the reduce step, the permission for the plots should be changed to be all +wrx.
 	rh.output <- "/wsc/tongx/Spatial/output"
 	lib.loc <- "/home/tongx/R_LIBS"
-}else if(par$machine == "gacrux"){
+}else if(par$machine == "gacrux" | par$machine == "adhara"){
 	local.datadir <- "/home/shaula/u16/tongx/Projects/Spatial/NCAR/RData"
 	local.output <- "/home/shaula/u16/tongx/Projects/Spatial/NCAR/output"
 	rh.datadir <- "/ln/tongx/Spatial/tmp"
+	local.raw <- "/home/shaula/u16/tongx/Projects/Spatial/NCAR/Raw"
 	#The output directory for saving plots should have all permission since the plots are written to 
     #the output directory by a user related to hadoop.
 	#In the reduce step, the permission for the plots should be changed to be all +wrx.
