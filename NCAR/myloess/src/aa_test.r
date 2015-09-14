4562205a021f2d6fb20e9693f89c0f389e4674e8
XiaosuTong <xiaosutong@gmail.com>
2015-09-14 13:13:52 -0400

    include mloessfkd for kd calculation

--- NCAR/myloess/src/aa_test.r
+++ NCAR/myloess/src/aa_test.r
@@ -3,8 +3,8 @@ source("aa_kdtree.R")
 
 r = 100
 set.seed(r)
-p = 3
-n = 2^20
+p = 4
+n = 2^25
 x = runif(n*p)  ## unif
 x = matrix(x,ncol=p)
 nb = 2^15
@@ -13,3 +13,21 @@ nb = 2^15
 t2 <- system.time({res2 <- cppkdtree(x, nb)})
 
 
+r = 100
+set.seed(r)
+p = 4
+n = 2^30
+x = runif(n*p)  ## unif
+x = data.frame(matrix(x,ncol=p))
+nb = 2^10
+
+t1 <- system.time({
+kdwhole <- my.loess2(
+	X1 ~ X2 + X3 + X4, 
+	data    = x,
+	degree  = 1, 
+	span    = 5/nb,
+	normalize = FALSE,
+	parametric = "X4"
+)$kd$vert2
+})
