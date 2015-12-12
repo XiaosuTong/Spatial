a96dfe34ced0010845272154a556db0c262e5142
XiaosuTong <xiaosutong@gmail.com>
2015-12-11 21:54:55 -0500

    add obsmonth and obsstation for a1950

---
+++ NCAR/rhcode/a1950/rh.a1950.obsmonth.R
@@ -0,0 +1,115 @@
+job <- list()
+job$map <- expression({
+  lapply(seq_along(map.keys), function(r) {
+    count <- sum(!is.na(map.values[[r]]$resp))
+    value <- data.frame(
+      count = count,
+      year = map.keys[[r]][1],
+      month = map.keys[[r]][2],
+      stringsAsFactors = FALSE
+    )
+    rhcollect(1, value)
+  })
+})
+job$reduce <- expression(
+  pre = {
+    combined <- data.frame()
+  },
+  reduce = {
+    combined <- rbind(combined, do.call(rbind, reduce.values))
+  },
+  post = { 
+    rhcollect(reduce.key, combined)
+  }
+)
+job$input <- rhfmt(
+  file.path(rh.root, par$dataset, "a1950", "bymonth"), 
+  type = "sequence"
+)
+job$output <- rhfmt(
+  file.path(rh.root, par$dataset, "a1950", "monthcount"), 
+  type = "sequence"
+)
+job$mapred <- list(
+  mapred.reduce.tasks = 1,  #cdh3,4
+  mapreduce.job.reduces = 1  #cdh5
+)
+job$readback <- FALSE
+job$combiner <- TRUE
+job$jobname <- file.path(rh.root, par$dataset, "a1950", "monthcount")
+job.mr <- do.call("rhwatch", job)
+
+rst <- rhread(file.path(rh.root, par$dataset, "a1950", "monthcount"))[[1]][[2]]
+
+library(plyr)
+library(lattice)
+rst$month <- factor(rst$month, levels = month.abb)
+date <- paste(rst$year, as.numeric(rst$month), "01", sep="-")
+rst$date <- as.POSIXct(strptime(date, format = "%Y-%m-%d"), format='%Y%m%d', tz="")
+rst <- arrange(rst, year, month)
+start <- head(rst$date, 1)
+end <- tail(rst$date, 1)
+
+
+trellis.device(
+  device = postscript, 
+  file = file.path(local.root, "output", "a1950.obs.month.ps"), 
+  color = TRUE, 
+  paper = "letter"
+)
+  b <- xyplot(log2(count) ~ date
+    , data = rst
+    , type = "l"
+    , xlab = list(label = "Date", cex = 1.5)
+    , ylab = list(label = "Log Base 2 Number of Observation", cex = 1.5)
+    , ylim = c(9, 13.5)
+    , aspect = "xy"
+    , scales = list(
+        y = list(cex = 1.2),
+        x = list(format = "%b %Y", at = c(seq(start, end, by="240 month")), cex = 1.2)
+      )   
+    , panel = function(x,y,...) {
+        panel.abline(v = seq(start, end, by="120 month"), h = seq(9.5, 13.5, 0.5), lwd = 0.5, col = "lightgray")
+        panel.abline(h = log2(Nstations), col = "red", lty=1, lwd = 1)
+        panel.xyplot(x,y,...)
+      }
+  )
+  print(b)
+dev.off()
+
+rst <- rhread("/ln/tongx/Spatial/tmp/tmax/a1950/bymonth")
+us.map <- map('state', plot = FALSE, fill = TRUE)
+trellis.device(
+  device = postscript, 
+  file = file.path(local.root, "output", paste(par$dataset, "a1950", "status", "ps", sep=".")), 
+  color = TRUE, 
+  paper = "letter"
+)
+for(i in 1:576) {
+  b <- xyplot( lat ~ lon
+    , data = rst[[i]][[2]]
+    , groups = factor(as.numeric(!is.na(resp)))
+    , xlab = list(label="Longitude", cex = 1.5)
+    , ylab = list(label="Latitude", cex = 1.5)
+    , sub = paste(rst[[i]][[1]][2], rst[[i]][[1]][1])
+    , key = list(
+        type = "p", 
+        text = list(label=c("missing","valid")),  
+        points = list(cex=1, pch=16, col=col[1:2]), 
+        columns = 2
+      )
+    , pch = 16
+    , cex = 0.4
+    , scales = list(
+        y = list(cex = 1.2),x = list(cex = 1.2)
+      )
+    , panel = function(x,y,...) {
+        panel.polygon(us.map$x,us.map$y)   
+        panel.xyplot(x,y,...)
+        panel.text(x=-123, y=26, labels=paste("Missing:", sum(is.na(rst[[i]][[2]]$resp))), adj=c(0,0))
+        panel.text(x=-123, y=25.5, labels=paste("Valid:    ", sum(!is.na(rst[[i]][[2]]$resp))), adj=c(0,1))
+      }
+  )
+  print(b)
+}
+dev.off()
