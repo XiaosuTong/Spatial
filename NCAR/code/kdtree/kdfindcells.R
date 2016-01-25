606ff7607cb652869cf9e4f06ae32078bbf779e2
XiaosuTong <xiaosutong@gmail.com>
2016-01-24 23:26:25 -0500

    modify the sample kd-tree from 128 to 512

--- NCAR/code/kdtree/kdfindcells.R
+++ NCAR/code/kdtree/kdfindcells.R
@@ -17,7 +17,7 @@ rhload(file.path(rh.root, "stationinfo", "USinfo.RData"))
 ## locs is all stations in a1950
 locs <- subset(UStinfo, station.id %in% stations.a1950)
 row.names(locs) <- 1:nrow(locs)
-rst <- cppkdtree(as.matrix(locs[,c(3,4)]), 100) ## return a kd-tree with 128 cells
+rst <- cppkdtree(as.matrix(locs[,c(3,4)]), 300) ## return a kd-tree with 512 cells
 rst <- data.frame(idx = rst[[5]], leaf = rst[[6]]) ## grab the row index and the leaf index 
 ## idx is a data.frame which sample one station for each cell
 idx <- ddply(
@@ -29,7 +29,7 @@ idx <- ddply(
   }
 )
 ### subset(rst, leaf==21)
-idx$V1[21] <- 7180
+#idx$V1[21] <- 7180
 ## places is stations with lon and lat in each cell
 places <- locs[row.names(locs) %in% idx[,2],]
 sample.a1950 <- data.frame(
@@ -52,11 +52,11 @@ a <- xyplot( lat ~ lon
   , data  = vert[[1]]
   , xlab  = list(label = "Longitude", cex=1.5)
   , ylab  = list(label = "Latitude", cex=1.5)
-  , key=list(
-    text = list(label=c("station location", "kdtree cell boundry")),
-    lines = list(pch = 16, cex = 0.7, lwd = 1, lty = 2, type = c("p","l"), col = c("blue", "red")), 
-    columns = 2
-    )
+#  , key=list(
+#    text = list(label=c("station location", "kdtree cell boundry")),
+#    lines = list(pch = 16, cex = 0.7, lwd = 1, lty = 2, type = c("p","l"), col = c("blue", "red")), 
+#    columns = 2
+#    )
   , scale = list(cex=1.2)
   , panel = function(x, y, ...) {
       panel.xyplot(x = places$lon, y = places$lat, cex = 0.7, col = "blue", 
@@ -65,18 +65,18 @@ a <- xyplot( lat ~ lon
       for(k in 1:nrow(places)){
         panel.text(
           places$lon[k], places$lat[k], sample.a1950$leaf[k], 
-          adj = c(0,0), col = col[1]
+          adj = c(0,0), col = "red", cex=0.5
         )
       }
-		  for(i in seq(1, nrow(vert[[1]]), by = 2)){
-			  panel.segments(x[i], y[i], x[i+1], y[i+1], col = "red", lty = 2, lwd = 0.7)
-		  }
-		  panel.segments(
-			  c(x[1], x[2]), c(y[1], y[2]), c(x[3], x[4]), c(y[3], y[4]), 
-			  col="red", lty=2, lwd = 0.7
-		  )
+#		  for(i in seq(1, nrow(vert[[1]]), by = 2)){
+#			  panel.segments(x[i], y[i], x[i+1], y[i+1], col = "red", lty = 2, lwd = 0.7)
+#		  }
+#		  panel.segments(
+#			  c(x[1], x[2]), c(y[1], y[2]), c(x[3], x[4]), c(y[3], y[4]), 
+#			  col="red", lty=2, lwd = 0.7
+#		  )
 	    panel.polygon(us.map$x,us.map$y)	
-	    panel.xyplot(x = x, y = y, col = "red", type = "p", pch = 16, cex = 0.3, ...)
+#	    panel.xyplot(x = x, y = y, col = "red", type = "p", pch = 16, cex = 0.3, ...)
 	  }
 )
 print(a)
